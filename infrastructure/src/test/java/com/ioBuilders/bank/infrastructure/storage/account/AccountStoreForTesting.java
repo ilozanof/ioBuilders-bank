932f8c8a435bb3ca0e2a1eba65f0e3cb7abefb1d
Ivan <35329568+ilozanof@users.noreply.github.com>
2023-06-13 19:07:43 +0200

    doc update and refactor

--- infrastructure/src/test/java/com/ioBuilders/bank/infrastructure/storage/account/AccountStoreForTesting.java
+++ infrastructure/src/test/java/com/ioBuilders/bank/infrastructure/storage/account/AccountStoreForTesting.java
@@ -1,13 +1,12 @@
 package com.ioBuilders.bank.infrastructure.storage.account;
 
-import com.ioBuilders.bank.infrastructure.storage.account.AccountJpaStore;
-import com.ioBuilders.bank.infrastructure.storage.account.AccountStore;
 import com.ioBuilders.bank.infrastructure.storage.user.UserJpaStore;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author i.fernandez@nchain.com
- * Copyright (c) 2018-2023 nChain Ltd
+ *
+ * This Store adds other meethods useful for testing but too "dangerous" to be included in the "main" codebase.
  */
 public class AccountStoreForTesting extends AccountStore {
     @Autowired
